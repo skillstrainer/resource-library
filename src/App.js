a5a729360155516f66ce0e15f17d49e12a9231f1
MALAY GOSWAMI <47104789+papermechanics@users.noreply.github.com>
2022-09-09 10:13:55 +0530

    select all added to multiselect

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import "./lib/styles.css";
 import React, { useState } from "react";
 import { CourseCard, CourseDetailPage } from "./lib/index";
 import { courses } from "./testing-data";
+import { MultiSelect } from "./lib/components/form-core/fields/MultiSelect";
 
 function App() {
+  const [select, setSelect] = useState([]);
   return (
     <div className="p-4">
-      <CourseDetailPage courseData={courses[0]} />
+      <MultiSelect
+        value={select}
+        onChange={(value) => setSelect(value)}
+        options={courses.map((course) => ({
+          label: course.displayName,
+          value: course.courseId,
+        }))}
+      />
+      {console.log(select)}
     </div>
   );
 }
--- src/lib/components/form-core/fields/MultiSelect.jsx
+++ src/lib/components/form-core/fields/MultiSelect.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import ReactSelect from "react-select";
 import { createPlugin } from "../utils";
 
@@ -7,13 +8,25 @@ export const MultiSelect = ({
   value,
   onChange,
   className,
+
   ...selectProps
 }) => {
   // set value for default selection
 
   // handle onChange event of the dropdown
-  const handleChange = (e) =>
-    onChange(Array.isArray(e) ? e.map((x) => x.value) : []);
+  const handleChange = (e) => {
+    if (e[e.length - 1]?.value === "#") {
+      onChange([]);
+      setSelectedAll(false);
+    } else {
+      if (e[0]?.value === "*" || e[e.length - 1]?.value === "*") {
+        onChange(options.map((x) => x.value));
+        setSelectedAll(true);
+      } else onChange(Array.isArray(e) ? e.map((x) => x.value) : []);
+    }
+  };
+
+  const [selectedAll, setSelectedAll] = useState(false);
 
   return (
     <div className={`mt-1 col-span-6 sm:col-span-3 ${className}`}>
@@ -21,7 +34,11 @@ export const MultiSelect = ({
         style={{ boxSizing: "border-box" }}
         {...selectProps}
         isMulti
-        options={options}
+        options={
+          selectedAll
+            ? [{ value: "#", label: "Deselect All" }, ...options]
+            : [{ value: "*", label: "Select All" }, ...options]
+        }
         value={options.filter(
           (obj) => Array.isArray(value) && value.indexOf(obj.value) > -1
         )}
