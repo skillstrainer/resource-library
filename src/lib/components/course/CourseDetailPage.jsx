40073ce2d20c69fb4d7f167f45990495632dce66
Abhishek Challa <abhishekchalla@Abhisheks-MacBook-Pro.local>
2022-10-12 15:52:38 +0530

    Separated course overview and purchase fragment from course detail page

--- src/lib/components/course/CourseDetailPage.jsx
+++ src/lib/components/course/CourseDetailPage.jsx
@@ -1,11 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 
-import { STRLContext } from "../../Context";
 import MultiLangBody from "../multi-lang/MultiLangBody";
 import MultiLangFieldMd from "../multi-lang/MultiLangFieldMd";
 import MultiLangFieldImage from "../multi-lang/MultiLangFieldImage";
 import MultiLangField from "../multi-lang/MultiLangField";
-import { stopPropagation } from "../../utils/dom";
+import CourseOverviewAndPurchaseFragment from "./CourseOverviewAndPurchaseFragment";
 
 // Icons
 import { Disclosure } from "@headlessui/react";
@@ -15,13 +14,8 @@ import { ChevronUpIcon } from "@heroicons/react/solid";
 import pageTopBg from "../../assets/image/page-top-bg.png";
 import emptyCertificate from "../../assets/image/certificate.jpg";
 import jobs from "../../assets/image/jobs.jpg";
-import newLogo from "../../assets/image/newLogo.svg";
-import { useState } from "react";
 
 function CourseDetailPage(props) {
-  const {
-    course: { getCoursePurchaseURL },
-  } = useContext(STRLContext);
   const {
     courseData,
     multiLangData,
@@ -36,34 +30,12 @@ function CourseDetailPage(props) {
   } = props;
 
   const {
-    courseId,
-    displayName,
-    categoryName,
     description,
-    courseImg,
-    cost,
-    discount,
-    students_enrolled,
-    nsqf_lvl,
     modules,
-    partners,
     videoUrl,
     certificateImageUrl,
     isMoodleCourse,
-    subscription_cost,
-    is_subscription,
-    interval,
-    course_type,
-
-    // Demo class
-    userHasRegisteredDemo,
-    onViewDemoDetails = () => {},
-    isDemoAvailable,
-    onBookDemo = () => {},
   } = courseData || {};
-
-  const [payingBySubscription, setPayingBySubscription] = useState(false);
-  const [paymentStarted, setPaymentStarted] = useState(false);
 
   return (
     <MultiLangBody _key={multiLangKey} data={multiLangData}>
@@ -76,228 +48,13 @@ function CourseDetailPage(props) {
             style={{ zIndex: "-1" }}
           />
           <div className="content mx-auto bg-white" style={{ zIndex: "1" }}>
-            <div className="flex justify-center">
-              <div className="px-4 py-10 md:px-10 shadow-xl w-full">
-                <div className="flex flex-between flex-col gap-5 md:flex-row">
-                  <div className="w-full md:w-2/5">
-                    <div class="relative">
-                      <img
-                        class="w-full object-cover object-center shadow-xl rounded-xl"
-                        src={courseImg}
-                        alt="course-img"
-                      />
-                      <div class="absolute top-4 left-2 font-semibold">
-                        <button
-                          className="bg-white text-xs font-semibold rounded-md p-1 shadow-lg border"
-                          onClick={goToCategoryPage}
-                        >
-                          {categoryName}
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-full md:w-3/5">
-                    <h2 className="text-2xl font-semibold leading-10 text-japanese_indigo mb-3">
-                      <MultiLangField name="display_name">
-                        {displayName}
-                      </MultiLangField>
-                    </h2>
-                    <div className="mb-6 text-sm">
-                      <div class="flex flex-row justify-between">
-                        <div className="flex items-center">
-                          <svg
-                            stroke="currentColor"
-                            fill="currentColor"
-                            stroke-width="0"
-                            viewBox="0 0 24 24"
-                            class="text-orange mx-1"
-                            height="1.4em"
-                            width="1.4em"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path
-                              fill-rule="evenodd"
-                              d="M16.67 13.13C18.04 14.06 19 15.32 19 17v3h4v-3c0-2.18-3.57-3.47-6.33-3.87z"
-                            ></path>
-                            <circle
-                              cx="9"
-                              cy="8"
-                              r="4"
-                              fill-rule="evenodd"
-                            ></circle>
-                            <path
-                              fill-rule="evenodd"
-                              d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4c-.47 0-.91.1-1.33.24a5.98 5.98 0 010 7.52c.42.14.86.24 1.33.24zM9 13c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"
-                            ></path>
-                          </svg>
-                          <span className="ml-2">
-                            {students_enrolled} students
-                          </span>
-                        </div>
-                        <a
-                          href="https://www.youtube.com/watch?v=riE-VMMXMHI"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <span className="red-dark mr-2">NSQF</span>
-                          <span>{nsqf_lvl || "NA"}</span>
-                        </a>
-                      </div>
-                    </div>
-                    <div className="text-md mb-3">
-                      <span className="font-semibold text-japanese_indigo mr-3 ">
-                        Course Structure:
-                      </span>
-                      <span>
-                        {course_type == 3
-                          ? "Physical Classes + Live Online Classes + Digital Content"
-                          : course_type == 2
-                          ? "Live Online Classes + Digital Content"
-                          : course_type == 1
-                          ? "Digital Content (Self Paced)"
-                          : "Self Paced Digital Content"}
-                      </span>
-                    </div>
-
-                    {is_subscription && !isPurchased && (
-                      <div className="text-md mb-3">
-                        <span className="font-semibold text-japanese_indigo mr-3 ">
-                          Course Payment type:
-                        </span>
-                        <span>
-                          <RadioButton
-                            label="One Time"
-                            value={!payingBySubscription}
-                            onChange={() => setPayingBySubscription(false)}
-                          />
-                          <RadioButton
-                            label="Installment"
-                            value={payingBySubscription}
-                            onChange={() => setPayingBySubscription(true)}
-                          />
-                        </span>
-                      </div>
-                    )}
-
-                    <div className="text-md mb-3">
-                      <span className="font-semibold text-japanese_indigo mr-3 ">
-                        {!payingBySubscription
-                          ? "Price:"
-                          : "Installment Price:"}
-                      </span>
-                      <span>
-                        {!payingBySubscription ? (
-                          <>
-                            <span
-                              className={discount ? "line-through mr-2" : ""}
-                            >
-                              {cost > 0 ? `₹ ${cost}` : "Free"}
-                            </span>
-                            {discount && discount > 0 && (
-                              <span>₹ {Number(cost) - Number(discount)}</span>
-                            )}
-                          </>
-                        ) : payingBySubscription ? (
-                          `₹${subscription_cost}/Month Upto ${interval} Months`
-                        ) : (
-                          ""
-                        )}
-                      </span>
-                    </div>
-
-                    {partners && (
-                      <div>
-                        <span className="font-semibold text-japanese_indigo mr-3 text-md">
-                          Certification Partners:
-                        </span>
-                        <div className="mx-2 flex flex-wrap mt-3 mb-3">
-                          {partners
-                            .concat([{ logo: newLogo }])
-                            .map(
-                              (p) =>
-                                (p && p.logo && (
-                                  <img src={p.logo} className="mr-3 h-6" />
-                                )) ||
-                                null
-                            )}
-                        </div>
-                      </div>
-                    )}
-
-                    <div className="flex gap-3">
-                      {isPurchased ? (
-                        <button
-                          className="w-full text-sm bg-red-dark hover:opacity-90 px-6 py-3 text-white rounded-lg md:w-auto"
-                          onClick={viewCourse}
-                        >
-                          View course
-                        </button>
-                      ) : (
-                        <button
-                          onClick={(e) => {
-                            stopPropagation(e);
-                            setPaymentStarted(true);
-                            payNow({
-                              payingBySubscription,
-                            })
-                              .catch(() => {})
-                              .then(setPaymentStarted);
-                          }}
-                          className="w-full text-sm bg-red-dark hover:opacity-90 px-4 py-2 text-white rounded-lg md:w-auto"
-                          disabled={paymentStarted}
-                        >
-                          {paymentStarted ? (
-                            "Please wait..."
-                          ) : !payingBySubscription ? (
-                            <>
-                              <span>Get Enrolled for </span>
-                              <span
-                                className={`font-bold ${
-                                  discount ? "line-through mr-2" : ""
-                                }`}
-                              >
-                                {cost > 0 ? `₹ ${cost}` : "Free"}
-                              </span>
-                              {discount && discount > 0 && (
-                                <span className="font-bold">
-                                  ₹ {Number(cost) - Number(discount)}
-                                </span>
-                              )}
-                            </>
-                          ) : (
-                            <>
-                              <span>Get Enrolled for </span>
-                              <span className="font-bold">
-                                ₹ {subscription_cost}
-                              </span>
-                            </>
-                          )}
-                        </button>
-                      )}
-                      {!isPurchased &&
-                        (userHasRegisteredDemo ? (
-                          <button
-                            onClick={() => onViewDemoDetails()}
-                            className="text-sm bg-red-dark hover:opacity-90 px-4 py-2 text-white rounded-lg"
-                          >
-                            Show demo class details
-                          </button>
-                        ) : (
-                          isDemoAvailable && (
-                            <button
-                              onClick={() => onBookDemo()}
-                              className="text-sm bg-red-dark hover:opacity-90 px-4 py-2 text-white rounded-lg"
-                            >
-                              Book demo
-                            </button>
-                          )
-                        ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <CourseOverviewAndPurchaseFragment
+              courseData={courseData}
+              onPaymentStarted={payNow}
+              viewCourse={viewCourse}
+              goToCategoryPage={goToCategoryPage}
+              isPurchased={isPurchased}
+            />
           </div>
         </div>
       </section>
@@ -424,21 +181,6 @@ function CourseDetailPage(props) {
                   }
                   className="w-full h-full"
                 />
-                {/* <p
-                  className="absolute w-full h-full top-0 left-0 text-center text-gray-600"
-                  style={{
-                    transform: "scale(0.6) translateY(50%)",
-                  }}
-                >
-                  student of <b>XYZ Institute</b> has successfully
-                  <br />
-                  cleared the assessment for the job role of
-                  <br />
-                  <b>{displayName}</b>
-                  <br />
-                  conforming to National Skills Qualifications Framework Level -{" "}
-                  {nsqf_lvl || "X"}
-                </p> */}
               </div>
             </div>
           </div>
@@ -486,19 +228,5 @@ function CourseDetailPage(props) {
     </MultiLangBody>
   );
 }
-
-const RadioButton = ({ label, value, onChange }) => {
-  return (
-    <label className="mr-5">
-      <input
-        type="radio"
-        className="mr-1"
-        checked={value}
-        onChange={onChange}
-      />
-      {label}
-    </label>
-  );
-};
 
 export default CourseDetailPage;
--- src/test.js
+++ src/test.js
@@ -1,81 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Form } from "./lib";
+import React from "react";
+import CourseDetailPage from "./lib/components/course/CourseDetailPage";
+import { courses } from "./testing-data";
 
 export default function Test(props) {
   return (
-    <div className="grid grid-cols-4 p-5">
-      <Form
-        formBuilder={FormBuilder}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
+    <div>
+      <CourseDetailPage
+        courseData={courses[0]}
+        goToCategoryPage={() => console.log("Going to category page")}
+        payNow={() => console.log("Making payment") || Promise.resolve(false)}
+        isPurchased={true}
+        viewCourse={() => console.log("Viewing course")}
       />
     </div>
   );
-}
-
-function FormBuilder(props) {
-  const [schema, setSchema] = useState({});
-
-  useEffect(() => {
-    setSchema({
-      name: {
-        type: "text",
-        label: "Name",
-      },
-      gender: {
-        type: "select",
-        label: "Gender",
-        options: [
-          {
-            label: "Male",
-            value: "male",
-          },
-          {
-            label: "Female",
-            value: "female",
-          },
-        ],
-      },
-      bio: {
-        type: "textarea",
-        label: "Bio",
-      },
-      hobbies: {
-        type: "multi-select",
-        options: [
-          {
-            label: "Art",
-            value: "art",
-          },
-          {
-            label: "Sports",
-            value: "sports",
-          },
-        ],
-      },
-      address: {
-        type: "address",
-        label: "Address",
-      },
-      dob: {
-        label: "DOB",
-        type: "date",
-      },
-      does_smoke: {
-        label: "Do you smoke?",
-        type: "boolean",
-      },
-      profile_pic: {
-        label: "Profile pic",
-        type: "file",
-      },
-      address: {
-        label: "Address",
-        type: "address",
-      },
-    });
-  }, []);
-
-  return schema;
 }
