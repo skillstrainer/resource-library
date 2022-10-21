6e6c3ff810881bec1a34aab238ae3faa0eef6619
MalayGoswami <gmalay17@gmail.com>
2022-10-21 13:15:33 +0530

    partner logo visible in course card

--- components/course/CourseCard.js
+++ components/course/CourseCard.js
@@ -13,6 +13,8 @@ var _dom = require("../../utils/dom");
 
 var _Context = require("../../Context");
 
+var _autoprefixer = require("autoprefixer");
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -27,6 +29,7 @@ function CourseCard(props) {
       cost,
       students_enrolled,
       discount,
+      partners,
       nsqf_lvl,
       duration,
       isMoodleCourse,
@@ -129,7 +132,13 @@ function CourseCard(props) {
     d: "M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4c-.47 0-.91.1-1.33.24a5.98 5.98 0 010 7.52c.42.14.86.24 1.33.24zM9 13c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"
   })), students_enrolled || 0))), /*#__PURE__*/_react.default.createElement("div", {
     className: "w-full text-right"
-  }, /*#__PURE__*/_react.default.createElement("p", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "flex flex-row ml-2 -mb-5"
+  }, partners && partners.map(partner => /*#__PURE__*/_react.default.createElement("img", {
+    src: partner.partner.logo,
+    className: "h-10 mr-4",
+    alt: "Partner Text"
+  })), " "), " ", /*#__PURE__*/_react.default.createElement("p", {
     className: "text-japanese_indigo text-sm mx-2"
   }, "See More Details >", " ")), /*#__PURE__*/_react.default.createElement("div", {
     className: "flex item-center justify-center w-full"
