72b7ca8b57c9b4541c152208384b80f1a42ffac7
MalayGoswami <gmalay17@gmail.com>
2022-12-01 14:42:13 +0530

    course card image- prepend s3 url

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.consts = void 0;
 const consts = {
   production: {
+    s3Url: "https://adminskillstrainerprod.s3.ap-south-1.amazonaws.com",
     webappUrl: "https://webapp.skillstrainer.in",
     adminPanelUrl: "https://admin.skillstrainer.in",
     marketingUrl: "https://skillstrainer.in",
@@ -13,6 +14,7 @@ const consts = {
     adminApiUrl: "https://adminapi.skillstrainer.in/api"
   },
   staging: {
+    s3Url: "https://adminskillstrainer.s3.ap-south-1.amazonaws.com",
     webappUrl: "https://webapp2.skillsscale.in",
     adminPanelUrl: "https://stageadmin.skillsscale.in",
     marketingUrl: "https://skillstrainer.in",
@@ -20,6 +22,7 @@ const consts = {
     adminApiUrl: "https://admin.skillsscale.in/api"
   },
   dev: {
+    s3Url: "https://adminskillstrainer.s3.ap-south-1.amazonaws.com",
     webappUrl: "http://localhost:3000",
     adminPanelUrl: "https://localhost:3000",
     marketingUrl: "https://localhost:4000",
