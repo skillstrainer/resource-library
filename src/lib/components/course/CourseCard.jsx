f6ff1f1b7656353b99e7eeadd13119c432c4115b
papermechanics <gmalay17@gmail.com>
2023-05-29 16:13:03 +0530

    course_card fix

--- src/lib/components/course/CourseCard.jsx
+++ src/lib/components/course/CourseCard.jsx
@@ -148,19 +148,21 @@ export default function CourseCard(props) {
         </div>
         <div className="flex flex-row  justify-between text-sm h-8 ">
           <p className="flex flex-row justify-center">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              className="text-orange mx-1 mt-1"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
-              <path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path>
-            </svg>
+            {duration && (
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 24 24"
+                className="text-orange mx-1 mt-1"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
+                <path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path>
+              </svg>
+            )}
             {duration}
           </p>
         </div>
