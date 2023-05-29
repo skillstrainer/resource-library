19415412b38ccca968dc478f7dc03c5c1bf3b652
nancybenjwal <benjwalnancy@gmail.com>
2023-05-29 16:51:13 +0530

    has certificate added

--- src/test/CourseDetail.jsx
+++ src/test/CourseDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { courses } from "../testing-data";
 import { CourseDetailPage } from "../lib/index";
+
 import { courseFormatter } from "../lib/utils/course";
 
 export default function CourseDetail(props) {
