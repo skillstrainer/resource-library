6ddf04eac1e7fa2292ab241587c0aba200d2a394
ashish <ashishsaraswal@yahoo.com>
2024-04-12 17:36:24 +0530

    Revert "checking for course image"
    This reverts commit 9c9e53adfd87eff59f12d3c9f5ce813ca678cb69.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import "./App.css";
 import "./lib/styles.css";
 import React, { useState } from "react";
 import Test from "./tests";
-import TestForm from "./test/TestForm";
-
 
 function App() {
-  return <TestForm/>;
+  return <Test />;
 }
 
 export default App;
--- src/tests.js
+++ src/tests.js
@@ -1,19 +1,17 @@
 import React from "react";
-// import CourseDetailPage from "./lib/components/course/CourseDetailPage";
-// import { courseCardData } from "./testing-data";
-// import CourseDetail from "./test/CourseDetail";
-// import ModalForm from "./test";
-// import CourseCard from "./lib/components/course/CourseCard";
-// import { CourseOverviewAndPurchaseFragment } from "./lib";
-import CreateUpdateFormBuilder from "./test/CreateUpdateFormBuilder";
+import CourseDetailPage from "./lib/components/course/CourseDetailPage";
+import { courseCardData } from "./testing-data";
+import CourseDetail from "./test/CourseDetail";
+import ModalForm from "./test";
+import CourseCard from "./lib/components/course/CourseCard";
+import { CourseOverviewAndPurchaseFragment } from "./lib";
 
 export default function Test(props) {
   return (
     <div>
-      {/* <CourseCard data={courseCardData[0]} /> */}
+      <CourseCard data={courseCardData[0]} />
       {/* <CourseOverviewAndPurchaseFragment courseData={courseCardData[0]} /> */}
-      {/* <CourseDetail /> */}
-      <CreateUpdateFormBuilder/>
+      <CourseDetail />
       {/* <ModalForm /> */}
     </div>
   );
